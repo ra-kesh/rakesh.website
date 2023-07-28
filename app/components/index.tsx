0ee505f7f87fc24ae5c4cdd9a1b856f84948059a
ra-kesh <iamrakeshkumar@pm.me>
2023-07-28 18:48:13 +0530

    feat:created components folder

---
+++ app/components/index.tsx
@@ -0,0 +1,7 @@
+import Container from "./container";
+import Content from "./content";
+import Description from "./description";
+import Title from "./title";
+import Wave from "./wave";
+
+export { Container, Content, Description, Title, Wave };
