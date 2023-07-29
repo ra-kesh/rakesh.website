e04367155a67573bb1bdbe01f068b08e284d470c
ra-kesh <iamrakeshkumar@pm.me>
2023-07-29 14:43:38 +0530

    feat:added layout component to home page

--- app/page.tsx
+++ app/page.tsx
@@ -8,38 +8,41 @@ import {
   Wave,
   Actions,
   Button,
+  Layout,
 } from "./components";
 
 import { ACTIONS } from "./data/actions";
 
 export default function Home() {
   return (
-    <Container>
-      <Content>
-        <Title>
-          Hello<Wave>👋 </Wave>, I am Rakesh!
-        </Title>
-        <Description>
-          A frontend developer who loves to work on React and all things
-          JavaScript/TypeScript.
-        </Description>
+    <Layout.Standard>
+      <Container.Home>
+        <Content.Home>
+          <Title>
+            Hello<Wave>👋 </Wave>, I am Rakesh!
+          </Title>
+          <Description>
+            A frontend developer who loves to work on React and all things
+            JavaScript/TypeScript.
+          </Description>
 
-        <Actions>
-          {ACTIONS.map((action) => {
-            return (
-              <Button
-                key={action.id}
-                href={action.href}
-                rel={action.external ? "noopener noreferrer" : ""}
-                target={action.external ? "_blank" : ""}
-              >
-                {action.icon}
-                {action.text}
-              </Button>
-            );
-          })}
-        </Actions>
-      </Content>
-    </Container>
+          <Actions>
+            {ACTIONS.map((action) => {
+              return (
+                <Button
+                  key={action.id}
+                  href={action.href}
+                  rel={action.external ? "noopener noreferrer" : ""}
+                  target={action.external ? "_blank" : ""}
+                >
+                  {action.icon}
+                  {action.text}
+                </Button>
+              );
+            })}
+          </Actions>
+        </Content.Home>
+      </Container.Home>
+    </Layout.Standard>
   );
 }
