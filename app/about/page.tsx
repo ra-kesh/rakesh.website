54a4bf59cc75a90552b02ec51db1d08d26fcbdc8
ra-kesh <iamrakeshkumar@pm.me>
2023-07-30 13:35:02 +0530

    feat;added initial boilerplate for about section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,46 @@
+"use client";
+
 import React from "react";
+import { Container, Content, Description, Layout, Title } from "../components";
 
-const page = () => {
-  return <div>page</div>;
-};
-
-export default page;
+export default function About() {
+  return (
+    <Layout.Standard>
+      <Container.About>
+        <Content.About>
+          <Description>
+            I am a frontend developer based out of India, who loves to work on
+            React and all things JavaScript/TypeScript. I have worked on some
+            backend also for my personal projects that used MERN stack .
+            <br />
+            <br />
+            Although I have a degree in computer science from a no tier college,
+            I have started coding during the pandemic out of boredom and got
+            hooked into it in time . Before getting into coding I was working as
+            a manager in a Photogrphy Firm managing clientle and contractual
+            photographers .
+            <br />
+            <br />
+            After learning the basics I enrolled myself in a six month bootcamp
+            taught by Tanay Pratap and made five fullstack projects within six
+            months after learning everything from scratch. I have also wrote
+            some blogs on Dev.to and Hashnode which has been read by
+            approximately more than 2500 developers yet.
+            <br />
+            <br />
+            After finishing with the bootcamp, I had a chance to work with
+            Himanshu Kapoor on this personal crypto project. He is a senior
+            software developer in GitLab and a indie hacker who have created a
+            lot of games in his spare time.
+            <br />
+            <br />
+            After finishing the crypto project, I got to know that Tanay Pratap
+            is looking for a Intern for this startup Invact and without thinking
+            twice I jumped on the opportunity of working with him and moved to
+            bangalore within two days .
+          </Description>
+        </Content.About>
+      </Container.About>
+    </Layout.Standard>
+  );
+}
