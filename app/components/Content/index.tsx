3c055e84d4ce569efcba6dea4ac7604ba7439899
ra-kesh <iamrakeshkumar@pm.me>
2023-07-31 13:39:56 +0530

    feat;added content for list items and project

--- app/components/Content/index.tsx
+++ app/components/Content/index.tsx
@@ -1,3 +1,5 @@
 export { Home } from "./Home.Component";
 export { Nav } from "./Navbar.Component";
 export { About } from "./About.Component";
+export { Project } from "./Project.Component";
+export { ListItem } from "./ListItem.Component";
