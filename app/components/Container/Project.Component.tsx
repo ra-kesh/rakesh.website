f386dd51def0bdec30add02adf6426a02de1f75a
ra-kesh <iamrakeshkumar@pm.me>
2023-08-02 00:05:21 +0530

    style:made changes to the work page syles

--- app/components/Container/Project.Component.tsx
+++ app/components/Container/Project.Component.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Project = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center items-center flex-col sm:flex-row min-h-screen">
+    <div className="flex justify-center  flex-col sm:flex-row min-h-screen">
       {children}
     </div>
   );
--- app/components/Content/Project.Component.tsx
+++ app/components/Content/Project.Component.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Project = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="max-w-md sm:max-w-lg md:sm:max-w-3xl lg:sm:max-w-5xl w-full mx-auto">
+    <div className="max-w-md sm:max-w-lg md:sm:max-w-3xl lg:sm:max-w-5xl w-full mx-auto mt-32">
       {children}
     </div>
   );
