13fcd2875ccdde4ec7deb9a84f33c81a714dabd8
ra-kesh <iamrakeshkumar@pm.me>
2023-08-02 16:55:51 +0530

    refactor:about page content and style

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,43 +1,54 @@
 "use client";
 
 import React from "react";
-import { Container, Content, Description, Layout } from "../components";
+import {
+  Container,
+  Content,
+  Description,
+  Layout,
+  TitleSecondary,
+} from "../components";
 
 export default function About() {
   return (
     <Layout.Standard>
       <Container.About>
         <Content.About>
+          <TitleSecondary>Hi There !</TitleSecondary>
           <Description>
-            I am a frontend developer based out of India, who loves to work on
-            React and all things JavaScript/TypeScript. I have worked on some
-            backend also for my personal projects that used MERN stack .
+            I am a frontend developer based out of India, who loves to craft
+            beautifully designed products on web.
+          </Description>
+          <TitleSecondary>My career so far ...</TitleSecondary>
+          <Description>
+            After completing my degree, I was working as a manager in a
+            Photography Firm managing clientele and contractual photographers
+            till pandemic.
             <br />
             <br />
-            Although I have a degree in computer science from a no tier college,
-            I have started coding during the pandemic out of boredom and got
-            hooked into it in time . Before getting into coding I was working as
-            a manager in a Photogrphy Firm managing clientle and contractual
-            photographers .
-            <br />
-            <br />
-            After learning the basics I enrolled myself in a six month bootcamp
-            taught by Tanay Pratap and made five fullstack projects within six
-            months after learning everything from scratch. I have also wrote
-            some blogs on Dev.to and Hashnode which has been read by
-            approximately more than 2500 developers yet.
+            During the pandemic, I got myself into a coding bootcamp taught by
+            Tanay Pratap (Senior Software Engineer, MicroSoft) out of boredom.
+            Where I have completed five full-stack projects within six months
+            from scratch. I have also wrote some blogs during that time which
+            have been read more than 2500 times already.
             <br />
             <br />
             After finishing with the bootcamp, I had a chance to work with
-            Himanshu Kapoor on this personal crypto project. He is a senior
-            software developer in GitLab and a indie hacker who have created a
-            lot of games in his spare time.
+            Himanshu Kapoor(Senior software Engineer, GitLab) on his personal
+            crypto project for three months .
             <br />
             <br />
-            After finishing the crypto project, I got to know that Tanay Pratap
-            is looking for a Intern for this startup Invact and without thinking
-            twice I jumped on the opportunity of working with him and moved to
-            bangalore within two days .
+            Then I joined Invact and built the whole frontend of their ADMIN
+            Portal and Teacher Portal which is now being used to manage
+            everything on the LMS. I was also the part of the Brand Reveal team
+            and created the new marketing site within weeks by effectively
+            collaborating with different stakeholders. I was awarded Employee Of
+            The Month also due to my tech and non-tech contribution for the
+            company.
+            <br />
+            <br />
+            After a eventful year at INVACT, I am looking out for my next
+            adventure right now.
           </Description>
         </Content.About>
       </Container.About>
