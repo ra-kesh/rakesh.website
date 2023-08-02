188638a6004f2e431238573decede8548db63e4d
ra-kesh <iamrakeshkumar@pm.me>
2023-08-02 16:55:11 +0530

    style:style changes for about pages

--- app/components/Container/About.Component.tsx
+++ app/components/Container/About.Component.tsx
@@ -1,5 +1,5 @@
 export const About = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex items-center justify-center mt-32">{children}</div>
+    <div className="flex items-center justify-center mt-40">{children}</div>
   );
 };
