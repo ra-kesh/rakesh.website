b767dffff6bb707348c2d49506c1aca9369c7481
ra-kesh <iamrakeshkumar@pm.me>
2023-08-02 16:53:59 +0530

    feat:created a generic util function for classNames

--- app/lib/util.ts
+++ app/lib/util.ts
@@ -1,3 +1,5 @@
-export function classNames(...classes: string[]) {
+export function classNames<T extends string | undefined>(
+  ...classes: T[]
+): string {
   return classes.filter(Boolean).join(" ");
 }
