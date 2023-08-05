1ee3f481cdf731d80ffe56044f44f669bdbc9648
ra-kesh <iamrakeshkumar@pm.me>
2023-08-06 01:31:35 +0530

    feat;added project card

---
+++ app/work/component/ProjectCard.Component.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { ProjectItem, WorkItem } from "@/app/types";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../../components";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import Link from "next/link";
+
+export const ProjectCard = ({
+  description,
+  title,
+  stack,
+  source,
+  link,
+  linkText,
+}: ProjectItem) => {
+  return (
+    <Card className="mb-5">
+      <CardHeader className="h-40">
+        <CardTitle className="text-lg font-bold text-gray-800">
+          {title}
+        </CardTitle>
+        <CardDescription>{description}</CardDescription>
+        <div className="flex flex-wrap gap-x-2">
+          {stack?.map((item, index) => {
+            return (
+              <span key={index} className=" text-xs text-gray-500 font-medium">
+                {item}
+              </span>
+            );
+          })}
+        </div>
+      </CardHeader>
+      <CardFooter className="flex gap-x-4 py-1">
+        <Link
+          href={source}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="rounded-full p-2 cursor-pointer hover:bg-gray-200 transition duration-300 ease-in-out "
+        >
+          <Icon
+            icon="feather:github"
+            className="text-gray-700 hover:text-gray-900"
+          />
+        </Link>
+        <Link
+          href={link}
+          rel="noopener noreferrer"
+          target="_blank"
+          className="cursor-pointer transform hover:translate-x-2 transition duration-300 ease-in-out "
+        >
+          <span className="font-bold text-gray-800 hover:text-gray-900">
+            {linkText} &rarr;
+          </span>
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+};
