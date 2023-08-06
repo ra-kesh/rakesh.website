8ce34f7250fd1680117386b924966b9000f29a7e
ra-kesh <iamrakeshkumar@pm.me>
2023-08-06 13:56:03 +0530

    refactor:removed unnecessary files

--- app/components/Container/index.tsx
+++ app/components/Container/index.tsx
@@ -2,5 +2,4 @@ export { Home } from "./Home.Component";
 export { Nav } from "./Navbar.Component";
 export { About } from "./About.Component";
 export { Project } from "./Project.Component";
-export { List } from "./List.Component";
 export { ListItem } from "./ListItem.Component";
