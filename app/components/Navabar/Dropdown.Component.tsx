80b8b3483c0667526a60f164ac67c47ca329c7b5
ra-kesh <iamrakeshkumar@pm.me>
2023-08-06 22:27:47 +0530

    refactor: added menu component to dropdown

--- app/components/Navabar/Dropdown.Component.tsx
+++ app/components/Navabar/Dropdown.Component.tsx
@@ -1,12 +1,14 @@
 import { classNames } from "@/app/lib/util";
 import { NavigationItem } from "@/app/types";
-import { Menu, Transition } from "@headlessui/react";
+import { Transition } from "@headlessui/react";
 import Link from "next/link";
 import React from "react";
+import { MenuGroup, MenuButton, MenuItem, MenuItems } from "..";
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 type DropdownProps = {
   children: React.ReactNode;
-  items: Array<NavigationItem>;
+  items: Array<Array<NavigationItem>>;
 };
 
 const DropDownTransition = ({ children }: { children: React.ReactNode }) => {
@@ -25,40 +27,48 @@ const DropDownTransition = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-const MenuItemsClassName =
-  "absolute z-10 right-0 origin-top-right w-56 mt-2 p-2 bg-gray-50 bg-opacity-75 backdrop-filter backdrop-blur-sm border border-gray-100 rounded-md shadow-lg focus:outline-none ring-black ring-opacity-5";
-
-const MenuButtonClassName =
-  "relative inline-block px-3 py-2 hover:bg-gray-50 text-gray-500 hover:text-gray-900 rounded-lg transition ease-in-out duration-300 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-primary-500";
-
 export const Dropdown = ({ children, items }: DropdownProps) => {
   return (
-    <Menu as="div" className="relative inline-block text-left">
-      <Menu.Button className={MenuButtonClassName}>{children}</Menu.Button>
+    <MenuGroup>
+      <MenuButton>{children}</MenuButton>
       <DropDownTransition>
-        <Menu.Items className={MenuItemsClassName}>
-          {items.map((item) => {
+        <MenuItems>
+          {items.map((section, index) => {
             return (
-              <Menu.Item key={item.id}>
-                {({ active }) => (
-                  <Link
-                    href={item.href}
-                    rel={item.external ? "noopener noreferrer" : ""}
-                    target={item.external ? "_blank" : ""}
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-500",
-                      "flex items-center px-4 py-3 text-sm font-medium tracking-wide rounded-md cursor-pointer transition ease-in-out duration-300 "
-                    )}
-                  >
-                    {item.icon}
-                    {item.text}
-                  </Link>
-                )}
-              </Menu.Item>
+              <section key={index}>
+                {section.map((item) => {
+                  return (
+                    <MenuItem key={item.id}>
+                      {({ active }) => (
+                        <Link
+                          href={item.href}
+                          rel={item.external ? "noopener noreferrer" : ""}
+                          target={item.external ? "_blank" : ""}
+                          className={classNames(
+                            active
+                              ? "bg-gray-100 text-gray-900"
+                              : "text-gray-500",
+                            "flex items-center px-4 py-3 text-sm font-medium tracking-wide rounded-md cursor-pointer transition ease-in-out duration-300 "
+                          )}
+                        >
+                          {item.icon}
+                          {item.text}
+                          {item.external && (
+                            <Icon
+                              icon="feather:external-link"
+                              className="ml-auto"
+                            />
+                          )}
+                        </Link>
+                      )}
+                    </MenuItem>
+                  );
+                })}
+              </section>
             );
           })}
-        </Menu.Items>
+        </MenuItems>
       </DropDownTransition>
-    </Menu>
+    </MenuGroup>
   );
 };
