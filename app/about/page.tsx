5c666191f999dd446fa9b3129d2675319d26a622
ra-kesh <iamrakeshkumar@pm.me>
2023-08-06 20:06:28 +0530

    refactor: change about page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 import {
   AboutContent,
   AboutContainer,
-  Description,
   Layout,
   TitleSecondary,
+  Description,
 } from "../components";
 
 export default function About() {
@@ -21,26 +21,25 @@ export default function About() {
           </Description>
           <TitleSecondary>My career so far ...</TitleSecondary>
           <Description>
-            After completing my degree, I was working as a manager in a
-            Photography Firm managing clientele and contractual photographers
-            till pandemic.
+            After completing my degree in 2019, I was working as a manager in a
+            Photography Firm till pandemic.
             <br />
             <br />
-            During the pandemic, I got myself into a coding bootcamp taught by
-            Tanay Pratap (Senior Software Engineer, MicroSoft) out of boredom.
-            Where I have completed five full-stack projects within six months
-            from scratch. I have also wrote some blogs during that time which
-            have been read more than 2500 times already.
+            During the pandemic, I discovered a new passion for coding and I got
+            myself into a coding bootcamp taught by Tanay Pratap
+            (SSE,MicroSoft). Where I have completed five full-stack projects
+            within six months from scratch. I have also written some blogs
+            during that time which have been read more than 2500 times already.
             <br />
             <br />
             After finishing with the bootcamp, I had a chance to work with
-            Himanshu Kapoor(Senior software Engineer, GitLab) on his personal
-            crypto project for three months .
+            Himanshu Kapoor (SSE,GitLab) on his personal crypto project for
+            three months .
             <br />
             <br />
-            Then I joined Invact and built the whole frontend of their ADMIN
+            Then I joined Invact and built the whole frontend of their Admin
             Portal and Teacher Portal which is now being used to manage
-            everything on the LMS. I was also the part of the Brand Reveal team
+            everything on the LMS. I was also the part of the Brand Reveal Team
             and created the new marketing site within weeks by effectively
             collaborating with different stakeholders. I was awarded Employee Of
             The Month also due to my tech and non-tech contribution for the
