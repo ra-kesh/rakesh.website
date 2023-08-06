e8a5e515400e82806f3807ef7a68028462824957
ra-kesh <iamrakeshkumar@pm.me>
2023-08-06 18:17:43 +0530

    feat:added navigation data

--- app/data/navigation.tsx
+++ app/data/navigation.tsx
@@ -1,7 +1,6 @@
 import { Icon } from "@iconify/react";
 import { v4 as uuid } from "uuid";
 import { NavigationItem } from "../types";
-import { ACTIONS } from ".";
 
 export const NAVIGATIONS: Array<NavigationItem> = [
   {
@@ -10,5 +9,37 @@ export const NAVIGATIONS: Array<NavigationItem> = [
     icon: <Icon icon="feather:home" className="mr-3" />,
     text: "Home",
   },
-  ...ACTIONS,
+  {
+    id: uuid(),
+    href: "/about",
+    icon: <Icon icon="feather:user" className="mr-3" />,
+    text: "About",
+  },
+  {
+    id: uuid(),
+    href: "/work",
+    icon: <Icon icon="feather:briefcase" className="mr-3" />,
+    text: "Work",
+  },
+  {
+    id: uuid(),
+    href: "https://github.com/ra-kesh",
+    external: true,
+    icon: <Icon icon="feather:github" className="mr-3" />,
+    text: "Github",
+  },
+  {
+    id: uuid(),
+    href: "https://twitter.com/kumarakeshh",
+    external: true,
+    icon: <Icon icon="feather:twitter" className="mr-3" />,
+    text: "Twitter",
+  },
+  {
+    id: uuid(),
+    href: "https://rakesh.website/cv",
+    external: true,
+    icon: <Icon icon="feather:file-text" className="mr-3" />,
+    text: "Resume",
+  },
 ];
