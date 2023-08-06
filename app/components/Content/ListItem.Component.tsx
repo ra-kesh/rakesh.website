9706392d2704167626ce9a6fcb38c99d0e915cf4
ra-kesh <iamrakeshkumar@pm.me>
2023-08-06 14:09:50 +0530

    refactor:removed unnecessary content component

--- app/components/Content/ListItem.Component.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const ListItem = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex flex-col sm:flex-row items-center justify-between p-4 sm:px-6">
-      {children}
-    </div>
-  );
-};
