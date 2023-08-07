e06b3bf9dbfca61411700892d41fed6a60c35e98
ra-kesh <iamrakeshkumar@pm.me>
2023-08-08 00:19:11 +0530

    feat;added config file for all the links and information

---
+++ app/lib/config.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: "rakesh kumar",
+  url: "https://rakesh.website",
+  ogImage: "",
+  description: "",
+  links: {
+    twitter: "https://twitter.com/kumarakeshh",
+    github: "https://github.com/ra-kesh",
+    linkedin: "https://www.linkedin.com/in/iamrakeshkumar/",
+    cv: "https://rakesh.website/cv",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
