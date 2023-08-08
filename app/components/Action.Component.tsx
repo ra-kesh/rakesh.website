2c7b722888b4393d5d35de9faec4e95f5f88e17d
ra-kesh <iamrakeshkumar@pm.me>
2023-08-08 22:07:11 +0530

    refactor:removed unnecessary component

--- app/components/Action.Component.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Action = ({ children }: { children: React.ReactNode }) => {
-  return <div className=" space-y-4 sm:space-x-4 ">{children}</div>;
-};
-
-export default Action;
