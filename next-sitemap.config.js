b67656d30ea32386e9e070d07467b123997bc8f0
ra-kesh <iamrakeshkumar@pm.me>
2023-08-09 23:23:47 +0530

    feat;added robots.txt and sitemaps dynamically

---
+++ next-sitemap.config.js
@@ -0,0 +1,38 @@
+const siteUrl = "https://www.rakesh.website";
+
+// add your private routes here
+const exclude = [];
+
+// Save crawling budget by not fetching SSG meta files
+const NEXT_SSG_FILES = [
+  "/*.json$",
+  "/*_buildManifest.js$",
+  "/*_middlewareManifest.js$",
+  "/*_ssgManifest.js$",
+  "/*.js$",
+];
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  exclude,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: NEXT_SSG_FILES,
+      },
+    ],
+  },
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
