0228af3d81724026b825f6e2a1e42b5f70e70cff
ra-kesh <iamrakeshkumar@pm.me>
2023-08-25 00:24:48 +0530

    chore:link updated

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -1,11 +1,11 @@
 import { v4 as uuid } from "uuid";
-import { ProjectItem, WorkItem } from "../types";
+import { ProjectItem } from "../types";
 
 export const PROJECTS: Array<ProjectItem> = [
   {
     id: uuid(),
     title: "MAD",
-    link: "https://moto-armor-depot.netlify.app/products",
+    link: "https://moto-armor-depot.netlify.app/",
     linkText: "rakesh.website/mad",
     source: "https://github.com/ra-kesh/armor",
     stack: ["react", "node", "express", "mongodb"],
@@ -32,24 +32,24 @@ export const PROJECTS: Array<ProjectItem> = [
     description:
       "An IDE that can fetch packages from UNPKG and bundle them together on the browser using ESBUILD-WASM.",
   },
-  {
-    id: uuid(),
-    title: "Torque",
-    link: "https://torquetv.netlify.app/",
-    linkText: "rakesh.website/torque",
-    source: "https://github.com/ra-kesh/torque",
-    stack: ["react", "node", "express", "mongodb"],
-    description:
-      "An Video Library App created using MERN stack as an assignment for NeogCamp",
-  },
-  {
-    id: uuid(),
-    title: "Rto quiz",
-    link: "https://rto-quiz.netlify.app/",
-    linkText: "rakesh.website/quiz",
-    source: "https://github.com/ra-kesh/rto-quiz",
-    stack: ["react", "node", "express", "mongodb", "typescript", "chakra ui"],
-    description:
-      "A Quiz App created using MERN stack as an assignment for NeogCamp",
-  },
+  // {
+  //   id: uuid(),
+  //   title: "Torque",
+  //   link: "https://torquetv.netlify.app/",
+  //   linkText: "rakesh.website/torque",
+  //   source: "https://github.com/ra-kesh/torque",
+  //   stack: ["react", "node", "express", "mongodb"],
+  //   description:
+  //     "An Video Library App created using MERN stack as an assignment for NeogCamp",
+  // },
+  // {
+  //   id: uuid(),
+  //   title: "Rto quiz",
+  //   link: "https://rto-quiz.netlify.app/",
+  //   linkText: "rakesh.website/quiz",
+  //   source: "https://github.com/ra-kesh/rto-quiz",
+  //   stack: ["react", "node", "express", "mongodb", "typescript", "chakra ui"],
+  //   description:
+  //     "A Quiz App created using MERN stack as an assignment for NeogCamp",
+  // },
 ];
